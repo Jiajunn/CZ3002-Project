f38857abd36d5dcf199e38e7e237d0f5e5b75257
JJ <L.jiajun1212@gmail.com>
2022-09-22 12:08:53 +0800

    hard coded nutrient log

--- FoodBuddy/app/components/FoodLog.js
+++ FoodBuddy/app/components/FoodLog.js
@@ -1,35 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import { Text, View, StyleSheet, Pressable, ScrollView } from "react-native";
 import Icon from "react-native-vector-icons/AntDesign";
 
 import FoodLogCard from "./FoodLogCard";
 
-function FoodLog() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.scroll}>
-        <ScrollView contentContainerStyle={styles.foodLogContainer}>
-          <FoodLogCard />
-          <FoodLogCard />
-          <FoodLogCard />
-          <FoodLogCard />
-          <FoodLogCard />
-        </ScrollView>
-      </View>
+export class FoodLog extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.scroll}>
+          <ScrollView contentContainerStyle={styles.foodLogContainer}>
+            <FoodLogCard />
+            <FoodLogCard />
+            <FoodLogCard />
+            <FoodLogCard />
+            <FoodLogCard />
+          </ScrollView>
+        </View>
 
-      <Pressable
-        style={({ pressed }) => [
-          {
-            backgroundColor: pressed ? "#7f7f7f" : "white",
-          },
-          styles.addFoodButton,
-        ]}
-      >
-        <Icon name="pluscircleo" size={25} />
-        <Text style={{ color: "black", paddingLeft: 20 }}> Add new meal</Text>
-      </Pressable>
-    </View>
-  );
+        <Pressable
+          style={({ pressed }) => [
+            {
+              backgroundColor: pressed ? "#7f7f7f" : "white",
+            },
+            styles.addFoodButton,
+          ]}
+        >
+          <Icon name="pluscircleo" size={25} />
+          <Text style={{ color: "black", paddingLeft: 20 }}> Add new meal</Text>
+        </Pressable>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
