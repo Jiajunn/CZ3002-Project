8278b2a25801463ec9b99b3ea9957967f2887621
JJ <L.jiajun1212@gmail.com>
2022-09-29 13:11:29 +0800

    Roshan's part

--- FoodBuddy/app/screens/RegisterScreen.js
+++ FoodBuddy/app/screens/RegisterScreen.js
@@ -1,1 +1,129 @@
+import * as React from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { Card } from "react-native-paper";
 
+export default function registerScreen1(props) {
+  return (
+    <View style={styles.container}>
+      <Card
+        style={{
+          backgroundColor: "#8F9467",
+          height: 140,
+          margin: -40,
+          marginBottom: 45,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 26,
+            marginTop: 87,
+            marginLeft: 100,
+            color: "white",
+          }}
+        >
+          Register
+        </Text>
+      </Card>
+      <View>
+        <Text
+          style={{
+            fontSize: 25,
+            marginLeft: 20,
+            fontWeight: "bold",
+            marginBottom: 50,
+          }}
+        >
+          Personal Information
+        </Text>
+        <View style={{ marginLeft: 40 }}>
+          <TouchableOpacity style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Username"
+              placeholderTextColor="#003f5c"
+            />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Password"
+              placeholderTextColor="#003f5c"
+            />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Weight(kg)"
+              placeholderTextColor="#003f5c"
+            />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Height(cm)"
+              placeholderTextColor="#003f5c"
+            />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Birth date"
+              placeholderTextColor="#003f5c"
+            />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder=" Gender"
+              placeholderTextColor="#003f5c"
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              backgroundColor: "#8F9467",
+              width: 235,
+              height: 45,
+              marginTop: 30,
+            }}
+          >
+            <TextInput
+              style={{ textAlign: "center", height: 50, fontSize: 20, flex: 1 }}
+              placeholder="NEXT"
+              placeholderTextColor="white"
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    padding: 0,
+  },
+  inputView: {
+    backgroundColor: "#F3EDED",
+    borderRadius: 30,
+    width: 235,
+    height: 45,
+    marginBottom: 20,
+    alignItems: "left",
+  },
+  TextInput: {
+    height: 50,
+    fontSize: 20,
+    flex: 1,
+    padding: 8,
+    marginLeft: 20,
+  },
+});
--- FoodBuddy/app/screens/UserProfileScreen.js
+++ FoodBuddy/app/screens/UserProfileScreen.js
@@ -1,12 +1,162 @@
-import React from "react";
-import { View, Text, SafeAreaView } from "react-native";
+import * as React from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
+import { Card } from "react-native-paper";
 
-function UserProfileScreen(props) {
+export default function UserProfileScreen(props) {
   return (
-    <SafeAreaView>
-      <Text>This is profile</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <View style={styles.banner}>
+        <Text style={styles.bannerText}> Profile </Text>
+      </View>
+      <View style={{ marginTop: 20 }}>
+        <Text
+          style={{
+            fontSize: 25,
+            marginLeft: 32,
+            fontWeight: "bold",
+            marginBottom: 20,
+          }}
+        >
+          User's Profile
+        </Text>
+        <ImageBackground
+          style={{ height: 150, width: 150, marginLeft: 135 }}
+          source={require("../assets/user.png")}
+        />
+
+        <Text
+          style={{
+            fontSize: 25,
+            marginLeft: 150,
+            alignItems: "centre",
+            fontWeight: "bold",
+            marginBottom: 15,
+          }}
+        >
+          Username
+        </Text>
+        <View
+          style={{
+            borderBottomColor: "black",
+            borderBottomWidth: 3,
+            marginBottom: 10,
+          }}
+        />
+        <View style={{ marginLeft: 30 }}>
+          <Text
+            style={{
+              fontSize: 20,
+              marginLeft: 10,
+              fontWeight: "bold",
+              marginBottom: 13,
+            }}
+          >
+            Weight
+          </Text>
+          <TouchableOpacity style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="72.3kg"
+              placeholderTextColor="#003f5c"
+            />
+          </TouchableOpacity>
+          <Text
+            style={{
+              fontSize: 20,
+              marginLeft: 10,
+              fontWeight: "bold",
+              marginBottom: 13,
+            }}
+          >
+            Height
+          </Text>
+          <TouchableOpacity style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="180cm"
+              placeholderTextColor="#003f5c"
+            />
+          </TouchableOpacity>
+          <Text
+            style={{
+              fontSize: 20,
+              marginLeft: 10,
+              fontWeight: "bold",
+              marginBottom: 13,
+            }}
+          >
+            Gender
+          </Text>
+          <TouchableOpacity style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Male"
+              placeholderTextColor="#003f5c"
+            />
+          </TouchableOpacity>
+          <Text
+            style={{
+              fontSize: 20,
+              marginLeft: 10,
+              fontWeight: "bold",
+              marginBottom: 13,
+            }}
+          >
+            Birthdate
+          </Text>
+          <TouchableOpacity style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="01/01/2022"
+              placeholderTextColor="#003f5c"
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
   );
 }
 
-export default UserProfileScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    padding: 0,
+  },
+  banner: {
+    backgroundColor: "#8F9467",
+    width: "100%",
+    height: "11%",
+    justifyContent: "flex-end",
+    paddingTop: "5%",
+  },
+  bannerText: {
+    fontSize: 30,
+    color: "white",
+    paddingLeft: 25,
+    marginBottom: 10,
+  },
+  inputView: {
+    backgroundColor: "#F3EDED",
+    borderRadius: 30,
+    width: 325,
+    height: 45,
+    marginBottom: 20,
+    alignItems: "left",
+  },
+  TextInput: {
+    height: 50,
+    fontSize: 20,
+    flex: 1,
+    padding: 8,
+    marginLeft: 20,
+  },
+});
