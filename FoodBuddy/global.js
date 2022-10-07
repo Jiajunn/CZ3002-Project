17110481a15daeb40852d3193b1eea4de0c128ca
yizejoseph <josephlohyize8614@gmail.com>
2022-10-08 01:32:57 +0800

    changed auth code

--- FoodBuddy/global.js
+++ FoodBuddy/global.js
@@ -1,6 +1,6 @@
 global.LoggedIn = false;
 
-global.IpAddress = "192.168.0.195";
+global.IpAddress = "192.168.50.239";
 //NTU ip = 10.27.17.84
 //JJ house ip = 192.168.1.117
 // yz school = 192.168.0.195
